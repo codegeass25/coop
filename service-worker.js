f2df9515c2953b8bbdc2c9bb69d34ea1072d756a
codegeass25 <angelroy.longakit@deped.gov.ph>
2026-09-08 08:07:15 +0800

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE='coopledger-github-pwa-v2.1.0-p01-hotfix-r3';
+const CACHE='coopledger-github-pwa-v2.1.0-p01-tb-notification-r4';
 const SHELL=['./index.html','./manifest.json','./icons/icon-192.png','./icons/icon-512.png'];
 self.addEventListener('install',event=>{event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(SHELL)).then(()=>self.skipWaiting()));});
 self.addEventListener('activate',event=>{event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k.startsWith('coopledger-')&&k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()));});
